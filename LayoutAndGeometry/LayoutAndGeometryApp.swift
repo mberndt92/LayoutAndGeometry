6e4893c29f8ebf348486f83af8a657a725f09135
Mberndt92 <maximilian_berndt@icloud.com>
2023-05-06 08:06:05 +0900

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Maximilian Berndt on 2023/05/06.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
